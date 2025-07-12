7540261aa3c9c433724d52170fb146b12d4987c7
Taanveer22 <taanveer469@gmail.com>
2025-07-13 00:44:31 +0600

    alhamdulliah all news loaded via catagory

--- script.js
+++ script.js
@@ -15,29 +15,88 @@ const loadCategory = async () => {
     "category-bar-container"
   );
   //   console.log(categoryBarContainer);
+  //   loop through every array data
   neededData.forEach((item) => {
     // console.log(item);
     const div = document.createElement("div");
     div.innerHTML = `
-          <button>${item.category_name}</button>
+          <button onclick = loadNews('${item.category_id}')>${item.category_name}</button>
                      `;
     categoryBarContainer.append(div);
   });
 };
 
-const loadNews = async () => {
+const loadNews = async (catId) => {
+  console.log(catId);
   const response = await fetch(
-    "https://openapi.programming-hero.com/api/news/category/01"
+    `https://openapi.programming-hero.com/api/news/category/${catId}`
   );
   //   console.log(response);
   const data = await response.json();
-//   console.log(data.data);
+  //   console.log(data.data);
   const allData = data.data;
+  //   console.log(allData);
+
+  const newsContainer = document.getElementById("news-container");
+  //   console.log(newsContainer);
+  //   clear feed after first loading
+  newsContainer.innerHTML = "";
+
+  //   loop through every array data
   allData.forEach((item) => {
-    console.log(item);
+    // console.log(item);
+    const div = document.createElement("div");
+    div.classList.add("single-news");
+    div.innerHTML = `
+        <div class="news-photo">
+            <img src=${item.image_url} />
+        </div>
+
+        <div class="news-info">
+
+            <div class="news-header">
+                <h4>
+                   ${item.title}
+                </h4>
+                <p class="news-badge">
+                   ${item.rating.badge}
+                   <sup>
+                        <span class="news-rating">${item.rating.number}</span>
+                   </sup>
+                </p>
+            </div>
+
+            <p>
+               ${item.details.slice(0, 300)}
+            </p>
+
+            <div class="news-footer">
+                <div class="author">
+                    <img class="author-img" src=${item.author.img} />
+                    <div class="author-info">
+                        <h2>${item.author.name}</h2>
+                        <p>${item.author.published_date}</p>
+                    </div>
+                </div>
+
+                <div class="views">
+                    <img class="view-img"
+                        src="https://uxwing.com/wp-content/themes/uxwing/download/health-sickness-organs/view-icon.png"
+                    />
+                    <p>${item.total_view}</p>
+                </div>
+                <div class="details-btn-container">
+                    <button class="details-btn">Details</button>
+                </div>
+            </div>
+
+        </div>
+
+    // `;
+    newsContainer.append(div);
   });
 };
 
 // load function invocation
 loadCategory();
-loadNews();
+loadNews("01");
